ec3602c18be971fda6c8f08168252ca6dd96bbae
Carlos Henrique Pinheiro Cordeiro <carloshenpincord@gmail.com>
2023-08-19 00:31:47 -0300

    Upload para salvar etapa do projeto. Atualmente no processo de redimensionamento para imagens maiores, faltando apenas redimensionar as linhas para imagem maior. Para posteriormente redimensionar linhas para imagem menor (apenas um único caso)

--- redimensionador-rgb/redimensionadorMaior.c
+++ redimensionador-rgb/redimensionadorMaior.c
@@ -12,7 +12,7 @@ void main() {
      * (2) Padrão 4k
      * (3) Padrão 8k
      */
-    short iTamanhoSelecionado = 1;
+    short iTamanhoSelecionado = 0;
     short iXImagem = 800;
     short iYImagem = 800;
     short aPixels[iXImagem][iYImagem][3];
@@ -20,6 +20,7 @@ void main() {
     short iX = 0;
     short iY = 0;
     char  sCor[200] = "";
+    short iTeste = 0;
     printf("\nMontando matriz de pixels...");
     for (short i = 0; i < 3; i++) //Ignorando valores de cabeçalho, resolução e tonalidade
     {
@@ -47,17 +48,7 @@ void main() {
             sSplit = strtok(NULL, " ");
         }
     }
-
-
-    /**
-     * IDEIAS:
-     * -PARA REDIMENSIONAR/AUMENTAR AS COLUNAS, SOMENTE DUPLICAR OS PIXEIS DAS COLUNAS PARA PREENCHER A LINHA TODA DO NOVO TAMANHO
-     * -PARA REDIMENSIONAR/AUMENTAR AS LINHAS, VERIFICAR QUAIS LINHAS DEVEM SER DUPLICADAS PARA BAIXO
-     * 
-     * -PRIMEIRO AUMENTAR AS LINHAS, DEPOIS AUMENTAR AS COLUNAS
-     * -NO ÚLTIMO PASSO, QUE É DO AUMENTO DAS COLUNAS, CONSTRUIR DIRETO NO ARQUIVO DA IMAGEM LINHA POR LINHA, JÁ COM AS LINHAS AUMENTADAS, ADICIONANDO OS NOVOS PIXEIS DAS COLUNAS CONFORME O NOVO TAMANHO.
-    */
-
+    fclose(oImg);
 
     /**
      * (0) Padrão 720p (HD)
@@ -65,36 +56,86 @@ void main() {
      * (2) Padrão 4k
      * (3) Padrão 8k
      */
-    short aTamanhosX[4] = {1280, 1920, 3840, 7680};
-    short aTamanhosY[4] = {720, 1080, 2160, 4320};
+    short aTamanhosX[4] = {720, 1080, 2160, 4320};
+    short aTamanhosY[4] = {1280, 1920, 3840, 7680};
     short iTamanhoX = aTamanhosX[iTamanhoSelecionado];
     short iTamanhoY = aTamanhosY[iTamanhoSelecionado];
 
-    
-    // printf("\nMontando nova imagem...");
-    // oNovaImagem = fopen("redimenRgbMaior.ppm", "w");
-    // fprintf(oNovaImagem, "P3\n");
-    // /**
-    //  * (0) Padrão 720p (HD)
-    //  * (1) Padrão 1080p Full HD 
-    //  * (2) Padrão 4k
-    //  * (3) Padrão 8k
-    //  */
-    // char aTamanhosResolucao[10][10] = {"1280 720","1920 1080","3840 2160","7680 4320"};
-    // fprintf(oNovaImagem, aTamanhosResolucao[iTamanhoSelecionado]);
-    // fprintf(oNovaImagem, "\n255\n");
-    // int iContador = 0;
-    // iX = 0;
-    // iY = 0;
-    // while (iContador < iTamanhoX*iTamanhoY)
-    // {
-    //     for (iY = 0; iY < iTamanhoY; iY++)
-    //     {
-    //         iContador++;
-    //         fprintf(oNovaImagem, "%d\n%d\n%d\n", aPixels[iX][iY][0], aPixels[iX][iY][1], aPixels[iX][iY][2]);
-    //     }
-    //     iX++;
-    // }
+    printf("\nRedimensionando colunas...");
+    oNovaImagem = fopen("redimenY.ppm", "w");
+    short iRed    = 0;
+    short iGreen  = 0;
+    short iBlue   = 0;
+    for (iX = 0; iX < iXImagem; iX++)
+    {
+        short iPixels = 0;
+        short iIndice = 0;
+        for (iY = 0; iY < iYImagem; iY++)
+        {
+            if (aPixels[iX][iY][0] != iRed || aPixels[iX][iY][1] != iGreen || aPixels[iX][iY][2] != iBlue)
+            {
+                iPixels = (iTamanhoY*iPixels)/iYImagem;
+                for (short p = 0; p < iPixels; p++)
+                {
+                    iIndice++;
+                    fprintf(oNovaImagem, "%d %d %d ", iRed, iGreen, iBlue);
+                }
+                iRed    = aPixels[iX][iY][0];
+                iGreen  = aPixels[iX][iY][1];
+                iBlue   = aPixels[iX][iY][2];
+                iPixels = 0;
+                continue;
+            }
+            iPixels++;
+        }
+        for (iIndice; iIndice < iYImagem; iIndice++)
+        {
+            fprintf(oNovaImagem, "%d %d %d ", iRed, iGreen, iBlue);
+        }
+        fprintf(oNovaImagem, "\n");
+    }
+    fclose(oNovaImagem);
+
+    oImg = fopen("Entrada_RGB.ppm", "r");
+    oNovaImagem = fopen("redimenY.ppm", "r");
+    printf("\nRedimensionando linhas...");
+    if (iTamanhoX > iXImagem) //Aumentar linhas
+    {
+        int   iLimite = iYImagem*3;
+        int   iIndice = 0;
+        short iLinhas = floor(iTamanhoX/iXImagem);
+        short aPixelsLinha[iLimite];
+        while (fgets(sCor, 200, oImg))
+        {
+            if (iIndice == iLimite)
+            {
+                break;
+            }
+            char *sSplit = strtok(sCor, " ");
+            while (sSplit != NULL)
+            {
+                if (strcmp(sSplit, "\n") != 0) 
+                {
+                    if (iY > 0 && iY % iYImagem == 0)
+                    {
+                        iY = 0;
+                        iX++;
+                    }
+                    aPixels[iX][iY][0] = atoi(sSplit);
+                    sSplit = strtok(NULL, " ");
+                    aPixels[iX][iY][1] = atoi(sSplit);
+                    sSplit = strtok(NULL, " ");
+                    aPixels[iX][iY][2] = atoi(sSplit);
+                    iY++;
+                }
+                sSplit = strtok(NULL, " ");
+            }
+        }
+    }
+    else//Diminuir linhas
+    {
+
+    }
     // fclose(oNovaImagem);
     fclose(oImg);
 }
