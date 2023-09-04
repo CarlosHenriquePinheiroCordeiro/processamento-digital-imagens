85b266d3f6b78fd26d889284a3cbadd8dc71b021
CarlosHenriquePinheiroCordeiro <carloshenpincord@gmail.com>
2023-09-04 10:22:02 -0300

    Exercício de converter uma imagem escala de cinza, para outra imagem em escala de cinza, contudo com pixeis binários, 0 ou 255

---
+++ conversor-pgm-binario/conversor-binario.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+
+FILE *oImg;
+FILE *oNovaImagem;
+
+void main() {
+    oImg        = fopen("Entrada_EscalaCinza.pgm", "r");
+    oNovaImagem = fopen("imagemBinariaConvertida.pgm", "w");
+    fprintf(oNovaImagem, "P1\n800 800\n");
+    char  sCor[10]  = "";
+    for (short i = 0; i < 3; i++) { //Ignorando valores de cabeçalho, resolução e tonalidade
+        fgets(sCor, 10, oImg);
+    }
+    while (fgets(sCor, 10, oImg))
+    {
+        fprintf(oNovaImagem, "%d\n", convertePixel(atoi(sCor)));
+    }
+    fclose(oNovaImagem);
+    fclose(oImg);
+}
+
+int convertePixel(int iPixel) {
+    int iPixelNovo = 0;
+    if (iPixel > 128) {
+        iPixelNovo = 255;
+    }
+    return iPixelNovo;
+}
