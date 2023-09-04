fe360d0847e367a5118b02b4287a6281e21b70e8
CarlosHenriquePinheiroCordeiro <carloshenpincord@gmail.com>
2023-09-04 10:41:11 -0300

    Correção do exercício

--- conversor-pgm-binario/conversor-binario.c
+++ conversor-pgm-binario/conversor-binario.c
@@ -9,7 +9,7 @@ FILE *oNovaImagem;
 void main() {
     oImg        = fopen("Entrada_EscalaCinza.pgm", "r");
     oNovaImagem = fopen("imagemBinariaConvertida.pgm", "w");
-    fprintf(oNovaImagem, "P1\n800 800\n");
+    fprintf(oNovaImagem, "P2\n800 800\n255\n");
     char  sCor[10]  = "";
     for (short i = 0; i < 3; i++) { //Ignorando valores de cabeçalho, resolução e tonalidade
         fgets(sCor, 10, oImg);
